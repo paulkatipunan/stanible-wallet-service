b15aa05b0829ea12b20aaec8840b31be471a233b
Barry Lavides <lavidesbarry@gmail.com>
2022-05-04 16:27:44 +0800

    fixed db connection string

--- database/connection.go
+++ database/connection.go
@@ -4,10 +4,10 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	// "os"
+	"os"
 
 	"github.com/joho/godotenv"
-	_"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 func CreateConnection() *sql.DB {
@@ -17,9 +17,7 @@ func CreateConnection() *sql.DB {
 		log.Fatal("Error loading .env file")
 	}
 
-	connStr := `postgres://admin:Xd8UCzVl5Z2U3C6IOJwONKZgRVWzTqz@34.142.140.9:5432/dev-wallet?sslmode=disable`
-	// db, err := sql.Open("postgres", os.ExpandEnv("$DB_USERNAME:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=$DB_SSLMODE"))
-	db, err := sql.Open("postgres", connStr)
+	db, err := sql.Open("postgres", os.Getenv("DB_CONNECTION"))
 
 	if err != nil {
 		panic(err)
