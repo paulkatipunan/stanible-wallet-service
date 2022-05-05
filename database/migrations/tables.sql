8eda9d6a0f186bb31fdccc4f27d6320cc6e3b815
Barry Lavides <lavidesbarry@gmail.com>
2022-05-05 10:06:01 +0800

    fiat_transations_assoc migration table

--- database/migrations/tables.sql
+++ database/migrations/tables.sql
@@ -48,14 +48,17 @@ CREATE TABLE fiat_transactions (
 );
 CREATE TRIGGER update_table_modtime BEFORE UPDATE ON fiat_transactions FOR EACH ROW EXECUTE PROCEDURE on_update_trigger();
 
-CREATE TABLE fiat_ramp_logs (
-	pk_fiat_ramp_logs_id UUID DEFAULT uuid_generate_v4(),
-	fk_fiat_transaction_id UUID NOT NULL REFERENCES fiat_transactions(pk_fiat_transaction_id),
+CREATE TABLE fiat_transations_assoc (
+	pk_fiat_transations_assoc_id UUID DEFAULT uuid_generate_v4(),
+
+	pk_sender_fiat_transaction_id UUID NOT NULL REFERENCES fiat_transactions(pk_fiat_transaction_id),
+	pk_receiver_fiat_transaction_id UUID NOT NULL REFERENCES fiat_transactions(pk_fiat_transaction_id),
+
 	ramp_tx_id VARCHAR UNIQUE NOT NULL,
 	description VARCHAR,
 	active BOOLEAN DEFAULT TRUE,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	PRIMARY KEY (pk_fiat_ramp_logs_id)
+	PRIMARY KEY (pk_fiat_transations_assoc_id)
 );
-CREATE TRIGGER update_table_modtime BEFORE UPDATE ON fiat_ramp_logs FOR EACH ROW EXECUTE PROCEDURE on_update_trigger();
+CREATE TRIGGER update_table_modtime BEFORE UPDATE ON fiat_transations_assoc FOR EACH ROW EXECUTE PROCEDURE on_update_trigger();
