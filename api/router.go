8a2c63da83b596dce7adc9ed4a5b2bf688845be7
Barry Lavides <lavidesbarry@gmail.com>
2022-05-20 23:37:13 +0800

    separate fiat buy endpoint, refactored

--- api/router.go
+++ api/router.go
@@ -43,6 +43,7 @@ func fiatDeposit(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&transactionPayload)
 
 	// Check balance cap
+	// Balance is from the receiving account
 	bal, err := utils.AccountBalance(transactionPayload.Receiver_user_id)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -79,131 +80,50 @@ func fiatDeposit(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// func fiatBuy(w http.ResponseWriter, r *http.Request) {
-// 	// Get payloads and assign fiat_transaction model
-// 	var errorFound bool = true
-// 	var transactionPayload models.Transaction_payload
-// 	json.NewDecoder(r.Body).Decode(&transactionPayload)
+func fiatBuy(w http.ResponseWriter, r *http.Request) {
+	// Get payloads and assign fiat_transaction model
+	var transactionPayload models.Transaction_payload
+	json.NewDecoder(r.Body).Decode(&transactionPayload)
 
-// 	// Begin tx
-// 	ctx := context.Background()
-// 	db := database.CreateConnection()
-// 	tx, err := db.BeginTx(ctx, nil)
-// 	defer db.Close()
+	// Check balance
+	// Balance is from the sending account
+	bal, err := utils.AccountBalance(transactionPayload.Sender_user_id)
 
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(utils.Response("error", err.Error(), nil))
+		return
+	}
 
-// 	// INDENTIFY TX TYPE to GET THE USER BALANCE
-// 	var tx_type string
-// 	sqlGetTransactionType := `SELECT type as type_name FROM transaction_types WHERE pk_transaction_type_id=$1`
-// 	db.QueryRow(sqlGetTransactionType, transactionPayload.Transaction_type_id).Scan(&tx_type)
+	if bal <= 0 || transactionPayload.Amount > int32(bal) {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(utils.Response("error", "Insuffucient balance", nil))
+		return
+	}
 
-// 	user_balance_owner := transactionPayload.Sender_user_id
+	// Get transaction type and transaction_type_id
+	pk_transaction_type_id, _ := utils.GetTransactionType(enums.BUY)
+	transactionPayload.Transaction_type_id = pk_transaction_type_id
 
-// 	// GET BALANCE
-// 	var balance []uint8
-// 	sqlGetUserBalance := `
-// 		SELECT
-// 			coalesce(SUM(ft.amount), 0) + (
-// 			SELECT
-// 				coalesce(SUM(ft.amount), 0) as balance
-// 			FROM
-// 				fiat_transactions ft
-// 			INNER JOIN
-// 				transaction_types tt
-// 				ON
-// 					ft.fk_transaction_type_id = tt.pk_transaction_type_id
-// 			LEFT JOIN
-// 				accounts a
-// 				ON
-// 					a.user_id = ft.fk_user_id
-// 			WHERE
-// 				ft.fk_user_id = $1 AND
-// 				tt.type IN ('withdraw', 'buy', 'refund') AND
-// 				a.active = true
-// 			) as balance
-// 		FROM
-// 			fiat_transactions ft
-// 		INNER JOIN
-// 			transaction_types tt
-// 			ON
-// 				ft.fk_transaction_type_id = tt.pk_transaction_type_id
-// 		LEFT JOIN
-// 			accounts a
-// 			ON
-// 				a.user_id = ft.fk_user_id
-// 		WHERE
-// 			ft.fk_user_id = $2 AND
-// 			tt.type = 'deposit' AND
-// 			a.active = true
-// 	`
-// 	db.QueryRow(sqlGetUserBalance, user_balance_owner, user_balance_owner).Scan(&balance)
+	// Check if sender and receiver addresses are both active
+	total_users := utils.ActiveSenderReceiver(transactionPayload.Sender_user_id, transactionPayload.Receiver_user_id)
+	if total_users < 2 {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(utils.Response("error", "Sender or receiver addresses are not active", nil))
+		return
+	}
 
-// 	bal, err := strconv.Atoi(strings.Split(string(balance), ".")[0])
+	// Insert fiat_transaction and fiat_transaction_assoc records
+	txResponse := utils.InsertFiatTransactionRecord(transactionPayload)
 
-// 	if err != nil {
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		json.NewEncoder(w).Encode(utils.Response("error", err.Error(), nil))
-// 		return
-// 	}
-
-// 	fmt.Println("TX TYPE: ", tx_type)
-// 	fmt.Println("Amount: ", transactionPayload.Amount)
-// 	fmt.Println("BALANCE: ", bal)
-
-// 	if tx_type == "buy" && bal <= 0 {
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		json.NewEncoder(w).Encode(utils.Response("error", "Insuffucient balance", nil))
-// 		return
-// 	}
-
-// 	if tx_type == "buy" && transactionPayload.Amount > int32(bal) {
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		json.NewEncoder(w).Encode(utils.Response("error", "Insuffucient balance", nil))
-// 		return
-// 	}
-
-// 	// INSERT TX
-// 	// Check if sender and receiver addresses are both active
-// 	sqlGetUsers := `
-// 		SELECT
-// 			COUNT(*) as total_users
-// 		FROM
-// 			accounts
-// 		WHERE
-// 			user_id IN (
-// 				$1,
-// 				$2
-// 			) AND
-// 			active = true
-// 	`
-
-// 	var total_users int
-// 	tx.QueryRow(sqlGetUsers, transactionPayload.Sender_user_id, transactionPayload.Receiver_user_id).Scan(&total_users)
-
-// 	if total_users < 2 {
-// 		// If not, rollback and return error
-// 		tx.Rollback()
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		json.NewEncoder(w).Encode(utils.Response("error", "Sender or receiver addresses are not active", nil))
-// 		return
-// 	} else {
-// 		errorFound = false
-// 	}
-
-// 	// Insert fiat_transaction record
-// 	txResponse := utils.InsertFiatTransactionRecord(transactionPayload)
-
-// 	if txResponse != nil {
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		json.NewEncoder(w).Encode(utils.Response("error", txResponse.Error(), nil))
-// 	} else {
-// 		w.WriteHeader(http.StatusOK)
-// 		json.NewEncoder(w).Encode(utils.Response("success", "", nil))
-// 	}
-// }
+	if txResponse != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(utils.Response("error", txResponse.Error(), nil))
+	} else {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(utils.Response("success", "", nil))
+	}
+}
 
 func walletBalance(w http.ResponseWriter, r *http.Request) {
 	db := database.CreateConnection()
@@ -344,7 +264,7 @@ func Router() *mux.Router {
 	routers.HandleFunc("/wallet/register", register).Methods("POST")
 
 	routers.HandleFunc("/wallet/fiat/deposit", fiatDeposit).Methods("POST")
-	// routers.HandleFunc("/wallet/fiat/buy", fiatBuy).Methods("POST")
+	routers.HandleFunc("/wallet/fiat/buy", fiatBuy).Methods("POST")
 	routers.HandleFunc("/wallet/fiat/balance/{user_id}", walletBalance).Methods("GET")
 
 	routers.HandleFunc("/wallet/transaction_types", transactionTypes).Methods("GET")
